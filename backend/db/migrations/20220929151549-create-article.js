b14906414fd2c039944d932387301fb1941302a5
Alina_Meishele <alina.meishele@gmail.com>
2022-09-29 19:27:37 +0300

    article  model filled in and associations created

--- backend/db/migrations/20220929151549-create-article.js
+++ backend/db/migrations/20220929151549-create-article.js
@@ -1,4 +1,3 @@
-'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Articles', {
@@ -6,25 +5,27 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       title: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false,
       },
       content: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Articles');
-  }
+  },
 };
--- backend/db/models/article.js
+++ backend/db/models/article.js
@@ -1,7 +1,4 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Article extends Model {
     /**
@@ -9,16 +6,51 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Comment, Like, User }) {
+      Article.hasMany(Comment, { foreignKey: 'articleId' });
+      Article.hasMany(Like, { foreignKey: 'articleId' });
+      Article.belongsToMany(User, {
+        through: Like,
+        foreignKey: 'articleId',
+        otherKey: 'likeId',
+      });
+      Article.belongsToMany(User, {
+        through: Comment,
+        foreignKey: 'articleId',
+        otherKey: 'commentId',
+      });
     }
   }
-  Article.init({
-    title: DataTypes.TEXT,
-    content: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Article',
-  });
+  Article.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Article',
+      tableName: 'Articles',
+    }
+  );
   return Article;
 };
