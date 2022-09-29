b0e8f4237feddf10fb80c2d2640dfd04e67b6afa
Alina_Meishele <alina.meishele@gmail.com>
2022-09-29 19:46:33 +0300

    chat  model filled in and associations created

--- backend/db/models/chat.js
+++ backend/db/models/chat.js
@@ -1,7 +1,4 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Chat extends Model {
     /**
@@ -9,17 +6,47 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ User }) {
+      Chat.belongsTo(User, { foreignKey: 'userId' });
     }
   }
-  Chat.init({
-    userId: DataTypes.INTEGER,
-    title: DataTypes.TEXT,
-    telegramUrl: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Chat',
-  });
+  Chat.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Users',
+        },
+      },
+      title: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      telegramUrl: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Chat',
+      tableName: 'Chats',
+    }
+  );
   return Chat;
 };
