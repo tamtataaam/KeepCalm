9ee52fba64ffd8b21b752dcf3d5c688a2cd33f0b
Alina_Meishele <alina.meishele@gmail.com>
2022-09-29 19:19:06 +0300

    recommendation  model filled in and associations created

--- backend/db/migrations/20220929151443-create-recommendations.js
+++ backend/db/migrations/20220929151443-create-recommendations.js
@@ -1,4 +1,3 @@
-'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Recommendations', {
@@ -6,25 +5,30 @@ module.exports = {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       conditionId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Conditions',
+        },
       },
       recommendation: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Recommendations');
-  }
+  },
 };
--- backend/db/models/recommendations.js
+++ backend/db/models/recommendations.js
@@ -1,24 +1,48 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Recommendations extends Model {
+  class Recommendation extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+    static associate({ Condition }) {
+      Recommendation.belongsTo(Condition, { foreignKey: 'conditionId' });
     }
   }
-  Recommendations.init({
-    conditionId: DataTypes.INTEGER,
-    recommendation: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Recommendations',
-  });
-  return Recommendations;
+  Recommendation.init(
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      conditionId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'Conditions',
+        },
+      },
+      recommendation: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Recommendation',
+      tableName: 'Recommendations',
+    }
+  );
+  return Recommendation;
 };
