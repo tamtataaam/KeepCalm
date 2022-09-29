1cfcf611c5fd171c1294f83bf4d0eecdb8335281
Alexander <raphop27@gmail.com>
2022-09-29 20:03:31 +0300

    add homepage

--- frontend/src/features/App.jsx
+++ frontend/src/features/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './Layout/Layout';
+import Main from './Main/Main';
 import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route path="home" element={<Main />} />
         {/* <Route path="/registration" element={<Registration />} />
         <Route path="/login" element={<Login />} />
         <Route path="/notfound" element={<Error404 />} />
