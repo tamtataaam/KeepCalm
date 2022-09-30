2ff7c81ab3263b797b7238dd501fe8fb8ddfc54a
Alexander <raphop27@gmail.com>
2022-09-30 16:37:35 +0300

    add form main box

--- frontend/src/features/Main/Main.jsx
+++ frontend/src/features/Main/Main.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import Login from '../Login/Login';
 import style from './Main.module.scss';
 
 function Main() {
   return (
-    <div className={style.main_img_div}>
-      <h3 className={style.welcome_text}>Добро пожаловать в KeepCalm</h3>
-      <img className={style.main_photo_ellipse} src="Ellipse@48.png" alt="Ellipse-Main" />
-      <img className={style.main_photo} src="Group@3x.png" alt="Main" />
-    </div>
+    <>
+      <div className={style.main_img_div}>
+        <h3 className={style.welcome_text}>Добро пожаловать в KeepCalm</h3>
+        <img className={style.main_photo_ellipse} src="Ellipse@48.png" alt="Ellipse-Main" />
+        <img className={style.main_photo} src="Group@3x.png" alt="Main" />
+      </div>
+      <div className={style.form_box}>
+        <Login />
+      </div>
+    </>
   );
 }
 export default Main;
