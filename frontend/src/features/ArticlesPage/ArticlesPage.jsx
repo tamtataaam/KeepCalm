9a3462ee21ebe3b9067c0a96f7761556ccb17174
Alexander <raphop27@gmail.com>
2022-09-30 22:25:22 +0300

    form input

--- frontend/src/features/ArticlesPage/ArticlesPage.jsx
+++ frontend/src/features/ArticlesPage/ArticlesPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+// import { useSelector } from 'react-redux';
 
 function ArticlesPage() {
-  const articles = useSelector((store) => store.articles);
-  console.log(articles);
+  // const articles = useSelector((store) => store.articles);
+  // console.log(articles);
 
   return (
     <h4>123</h4>
