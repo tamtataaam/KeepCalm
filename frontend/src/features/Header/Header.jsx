af9e5a19db680a1591e45269e62eb3bcb1e11a87
Alexander <raphop27@gmail.com>
2022-09-30 15:46:09 +0300

    edit menu navbar

--- frontend/src/features/Header/Header.jsx
+++ frontend/src/features/Header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
+// import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Tooltip from '@mui/material/Tooltip';
+import Logout from '@mui/icons-material/Logout';
 import style from './Header.module.scss';
 
 function Header() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <header className={style.header}>
       <div>
@@ -12,9 +29,69 @@ function Header() {
         </a>
       </div>
       <div className={style.button_nav}>
-        <Button>Саня</Button>
-        <Avatar className={style.avatar_nav} alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-        <Button>Выйти</Button>
+        <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+          <Typography style={{ color: 'white' }} sx={{ minWidth: 10 }}>Sanya</Typography>
+          <Tooltip title="Account settings">
+            <IconButton
+              onClick={handleClick}
+              size="small"
+              sx={{ ml: 2 }}
+              aria-controls={open ? 'account-menu' : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? 'true' : undefined}
+            >
+              <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>
+            </IconButton>
+          </Tooltip>
+        </Box>
+        <Menu
+          anchorEl={anchorEl}
+          id="account-menu"
+          open={open}
+          onClose={handleClose}
+          onClick={handleClose}
+          PaperProps={{
+            elevation: 0,
+            sx: {
+              overflow: 'visible',
+              filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+              mt: 1.5,
+              '& .MuiAvatar-root': {
+                width: 32,
+                height: 32,
+                ml: -0.5,
+                mr: 1,
+              },
+              '&:before': {
+                content: '""',
+                display: 'block',
+                position: 'absolute',
+                top: 0,
+                right: 14,
+                width: 10,
+                height: 10,
+                bgcolor: 'background.paper',
+                transform: 'translateY(-50%) rotate(45deg)',
+                zIndex: 0,
+              },
+            },
+          }}
+          transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+          anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        >
+          <MenuItem>
+            <Avatar />
+            {' '}
+            Мой аккаунт
+          </MenuItem>
+          <Divider />
+          <MenuItem>
+            <ListItemIcon>
+              <Logout fontSize="small" />
+            </ListItemIcon>
+            Выйти
+          </MenuItem>
+        </Menu>
       </div>
     </header>
   );
--- frontend/src/features/Main/Main.jsx
+++ frontend/src/features/Main/Main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './Main.css';
+import style from './Main.module.scss';
 
 function Main() {
   return (
-    <div className="main-img-div">
-      <h3 className="welcome-text">Лучшее приложение в мире</h3>
-      <img className="main-photo" src="Group@3x.png" alt="Main" />
+    <div className={style.main_img_div}>
+      <h3 className={style.welcome_text}>Добро пожаловать в KeepCalm</h3>
+      <img className={style.main_photo_ellipse} src="Ellipse@48.png" alt="Ellipse-Main" />
+      <img className={style.main_photo} src="Group@3x.png" alt="Main" />
     </div>
   );
 }
