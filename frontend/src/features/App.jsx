41de185eb57bda9b969d5970ec0b9fd3a4cbec96
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-10-01 13:56:55 +0300

    fix styles for main page

--- frontend/src/features/App.jsx
+++ frontend/src/features/App.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react/jsx-no-useless-fragment */
+/* eslint-disable react/jsx-no-undef */
 import React, { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { loadAsyncExercises } from '../store/Exercises/exerciseSlice';
 import { loadAsyncArticles } from '../store/articlesSlice/articlesSlice';
 import { loadUser } from '../store/userSlice/userSlice';
@@ -11,15 +13,17 @@ import UserPage from './UserPage/UserPage';
 import Mood from './Mood/MoodSmiley';
 import Registration from './Registration/Registration';
 import Login from './Login/Login';
-import Main from './Main/Main';
 import Exercises from './Exercises/Exercises';
 import ExerciseFullInformation from './Exercises/ExerciseFullInformation';
 import ArticlesPage from './ArticlesPage/ArticlesPage';
 import OneArticlePage from './ArticlesPage/OneArticlePage';
 import ChatsPage from './Chats/ChatsPage';
+import MainAuth from './Main/MainAuth';
 import './App.css';
 
 function App() {
+  const { isUser } = useSelector((store) => store.user);
+
   // Alinas's part start
   const dispatch = useDispatch();
   useEffect(() => {
@@ -32,25 +36,31 @@ function App() {
   }, []);
   // Alinas's part end
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Main />} />
-        <Route path="/lk/:login" element={<UserPage />} />
-        <Route path="/mood" element={<Mood />} />
-        <Route path="/exercises" element={<Exercises />} />
-        <Route path="/exercises/:id" element={<ExerciseFullInformation />} />
-        <Route path="/registration" element={<Registration />} />
-        <Route path="/articles" element={<ArticlesPage />} />
-        <Route path="/articles/:id" element={<OneArticlePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/chats" element={<ChatsPage />} />
-
-        {/* <Route path="/registration" element={<Registration />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/notfound" element={<Error404 />} />
-        <Route path="*" element={<Error404 />} /> */}
-      </Route>
-    </Routes>
+    <>
+      {isUser ? (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            {/* <Route index element={<Main />} /> */}
+            {/* <Route index element={<Exercises />} /> */}
+            <Route path="/lk" element={<UserPage />} />
+            <Route path="/mood" element={<Mood />} />
+            <Route path="/exercises" element={<Exercises />} />
+            <Route path="/exercises/:id" element={<ExerciseFullInformation />} />
+            <Route path="/articles" element={<ArticlesPage />} />
+            <Route path="/articles/:id" element={<OneArticlePage />} />
+            <Route path="/chats" element={<ChatsPage />} />
+          </Route>
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MainAuth />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registration" element={<Registration />} />
+          </Route>
+        </Routes>
+      )}
+    </>
   );
 }
 
