fe2a060901a5555dcfff50b307a59d019b3c3c49
Tamara Gasparyan <tamara_gasparyan12@mail.ru>
2022-10-02 18:26:51 +0300

    add styles for mood page

--- frontend/src/features/Mood/MoodSmiley.jsx
+++ frontend/src/features/Mood/MoodSmiley.jsx
@@ -2,10 +2,13 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { addSmiley } from '../../store/moodSlice/moodSlice';
+import LoadingPage from '../LoadingPage/LoadingPage';
 import './mood.css';
 
 function Mood() {
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   // const userIdmoodId = useSelector((prev) => prev.mood);
   // console.log(userIdmoodId);
@@ -15,16 +18,25 @@ function Mood() {
   // useEffect(() => {
   //   dispatch(loadSmiley());
   // }, []);
+
   return (
-    <div className="smile_container">
-      {moodSm.map((el) => (
-        <div>
-          <div onClick={() => getSmiley(el.id)}>
-            <img className="smile_img" src={el.moodUrl} alt="фото_эмодзи" />
+    <div className="mood_container">
+      { moodSm ? (
+        <>
+          <h1 className="mood_h1">Какое у тебя сейчас настроение?</h1>
+          <h2 className="mood_h2">Выбери свое настроение на сегодня</h2>
+          <div className="smile_container">
+            {moodSm.map((el) => (
+              <div onClick={() => getSmiley(el.id)}>
+                <img className="smile_img" src={el.moodUrl} alt="smile" />
+              </div>
+            ))}
           </div>
-          <div>{el.mood}</div>
-        </div>
-      ))}
+          <button className="mood_btn" type="button" onClick={() => navigate('/exercises')}>Готово</button>
+        </>
+      ) : (
+        <LoadingPage />
+      )}
     </div>
   );
 }
