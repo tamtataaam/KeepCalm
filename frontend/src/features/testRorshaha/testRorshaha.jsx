53bbfb2f96eac363000eaed23027344cd6dc3de5
Данил Ледяев <danilledaev@mbp-danil.local>
2022-10-04 16:21:50 +0300

    first_page_test

--- frontend/src/features/testRorshaha/testRorshaha.jsx
+++ frontend/src/features/testRorshaha/testRorshaha.jsx
@@ -4,7 +4,20 @@ import style from './testRorshaha.module.scss';
 function testRorshaha() {
   return (
     <div>
-      <img src="frontend/public/1Rorshar.jpeg" alt="" />
+      <div className={style.main_container}>
+        <div className={style.container}>
+          <div className={style.img_face_test}>
+            <img src="https://testometrika.com/upload/uf/3ce/3cecd2c79d3224c1df1c1bf982778abf.svg" />
+          </div>
+          {/* <img src="1Rorshar.jpeg" alt="1 вопрос" /> */}
+          <h1 className={style.h1}>
+            Чернильные пятна Роршаха: все ли в порядке с вашей психикой?
+          </h1>
+          <button type="button" className={style.button}>
+            Пройти тест
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
