ccb56d2f723288553c563b874e9d17f86d5b8179
Alina_Meishele <alina.meishele@gmail.com>
2022-10-06 16:01:32 +0300

    filter by title done

--- frontend/src/features/Chats/ChatsPage.jsx
+++ frontend/src/features/Chats/ChatsPage.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-restricted-syntax */
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import style from './Chats.module.scss';
 import ChatItem from './ChatItem';
@@ -13,24 +13,6 @@ function ChatsPage() {
   const filteredChatsTitle = allChats.filter((chat) =>
     chat.title.toLowerCase().includes(value.toLowerCase())
   );
-
-  const filteredChatsDecsription = allChats.filter((chat) =>
-    chat.description.toLowerCase().includes(value.toLowerCase())
-  );
-
-  const totalFiltered = filteredChatsTitle.concat(filteredChatsDecsription);
-  const set = new Set(totalFiltered);
-  const filteredByTwoWilters = [];
-  const foo = () => {
-    for (const elem of set) {
-      filteredByTwoWilters.push(elem);
-    }
-  };
-  useEffect(() => {
-    foo();
-    console.log(value);
-    console.log(filteredByTwoWilters);
-  }, [value]);
 
   return (
     <>
@@ -46,23 +28,14 @@ function ChatsPage() {
       </button>
       <div className={style.container}>
         <div className={style.chats_container}>
-          {filteredByTwoWilters.length ? (
-            filteredByTwoWilters.map((chat) => (
+          {filteredChatsTitle.length ? (
+            filteredChatsTitle.map((chat) => (
               <ChatItem key={chat.id} chat={chat} />
             ))
           ) : (
             <h2>Список пуст</h2>
           )}
         </div>
-        {/* <div className={style.chats_container}>
-          {filteredByTwoWilters.length ? (
-            filteredByTwoWilters.map((chat) => (
-              <ChatItem key={chat.id} chat={chat} />
-            ))
-          ) : (
-            <h2>Список пуст</h2>
-          )}
-        </div> */}
 
         <div className={style.addchat_button_container}>
           <AddChatPage />
